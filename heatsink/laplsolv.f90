4480a29b868b957ed91b4095f07c08dd72d75308
Alexander Norström <alexander@norstrom.org>
2011-05-17 12:10:48 +0000

    labb3: heatsink funkar

--- heatsink/laplsolv.f90
+++ heatsink/laplsolv.f90
@@ -9,28 +9,42 @@ use omp_lib
   integer, parameter                  :: n=1000, maxiter=1000
   double precision,parameter          :: tol=1.0E-3
   double precision,dimension(0:n+1,0:n+1) :: T
-  double precision,dimension(n)       :: tmp1,tmp2,tmp3,Tlast
+  double precision,dimension(n)       :: tmp1,tmp2,tmp3,Terr
   double precision                    :: error,x, err
-  double precision                                :: t1,t0
-  integer                             :: i,j,k, Tl
+  double precision                    :: t1,t0
+  integer                             :: i,j,k, Tl, id, last
   character(len=20)                   :: str
+
+  integer                             :: nthreads, istart, iend
   
   ! Set boundary conditions and initial values for the unknowns
   T=0.0D0
   T(0:n+1 , 0)     = 1.0D0
   T(0:n+1 , n+1)   = 1.0D0
   T(n+1   , 0:n+1) = 2.0D0
-  ! T(y, x) Konstiga sak...
 
   ! Solve the linear system of equations using the Jacobi method
-!  call cpu_time(t0)
+
   t0 = omp_get_wtime();
-  !$omp parallel shared(T, Ti, Tlast, error, k)
+
+  !$omp parallel private(id, istart, iend, tmp1, tmp2, tmp3, k, j)
+
+  !$omp single
+  nthreads = omp_get_num_threads()
+  lines_per_thread = n / nthreads + 1
+  !$omp end single
+
+  id = omp_get_thread_num();
+  istart = id * lines_per_thread + 1
+  iend = min((id +1) * lines_per_thread, n)
+
+  !$omp critical
+  write (*,*) 'Hello from', id, istart, iend
+  !$omp end critical
+
   do k=1,maxiter
-     
+    
      tmp1=T(1:n,0)
-     Ti = 0
-     Tlast = T(1:n,0)
      error=0.0D0
      
 !     do j=1,n
@@ -40,36 +54,40 @@ use omp_lib
 !        tmp1=tmp2
 !     end do
 
-     !$omp do ordered private(tmp1, tmp2, tmp3, err) reduction(max: error)
-     do j=1,n
-        !$omp ordered
-        tmp3 = T(1:n,j-1)
+     tmp1 = T(1:n,istart - 1)
+     tmp3 = T(1:n,iend + 1)
 
+     !$omp barrier
+     !$omp flush
+
+     do j = istart, iend - 1
         tmp2 = T(1:n,j)
-        tmp1 = (T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp3)/4.0D0
-        !$omp end ordered
-        err = maxval(abs(tmp2 - tmp1))
+        T(1:n,j) = (T(0:n-1,j) + T(2:n+1,j) + T(1:n,j+1) + tmp1)/4.0D0
 
-        
-        error = max(error, err)
-       
+        !$omp critical
+        error = max(error, maxval(abs(tmp2 - T(1:n,j))))
+        !$omp end critical
 
-        !$omp ordered
-
-        T(1:n,j) = tmp1
-        !$omp end ordered
-        
+        tmp1 = tmp2
      end do
-     !$omp end do
-  
-     
+
+     ! Specialfall för gränsgrejer
+     tmp2 = T(1:n, iend)
+     T(1:n,iend) = (T(0:n-1,iend) + T(2:n+1,iend) + tmp3 + tmp1)/4.0D0
+     !$omp critical
+     error = max(error, maxval(abs(tmp2 - T(1:n,iend))))
+     !$omp end critical
+
+     !$omp barrier
+     !$omp flush
+
      if (error<tol) then
         exit
      end if
-     
+     !$omp barrier
   end do
   !$omp end parallel  
-!  call cpu_time(t1)
+
   t1 = omp_get_wtime();
 
   write(unit=*,fmt=*) 'Time:',t1-t0,'Number of Iterations:',k
